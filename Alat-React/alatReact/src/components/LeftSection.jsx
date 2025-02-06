885f63a0edcc9c600edadbd6b7623fc912e85c42
Toluadeife <jossydarry@yahoo.com>
2025-02-06 13:32:15 +0100

    m

--- Alat-React/alatReact/src/components/LeftSection.jsx
+++ Alat-React/alatReact/src/components/LeftSection.jsx
@@ -3,15 +3,15 @@ function LeftSection(){
 		<section className="left-section">
 			<img src="alatReact\src\assets\alat assets\alat img.jpg" alt="" />
       <div className="contact-info">
-        <h4>Talk to us</h4>
-        <p className="we">We are happy to assist</p>
+        {/* <h4>Talk to us</h4> */}
+        {/* <p className="we">We are happy to assist</p> */}
         <div className="icons">
           <div className="email-icons">
-            <p>Email: help@alat.ng</p>
-            <p>Phone: 070022552528</p>
+            {/* <p>Email: help@alat.ng</p>
+            <p>Phone: 070022552528</p> */}
           </div>
           <div className="social-icons">
-            <p>Follow us on:</p>
+            {/* <p>Follow us on:</p> */}
             <span>Instagram | Facebook | Twitter</span>
           </div>
         </div>
